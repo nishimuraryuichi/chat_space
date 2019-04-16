6ada830c0d194d04b1e10d31b5bd3fd13f9e3ea9
ryuuuuuu1 <ryu@ryunoMacBook-Air.local>
2019-04-16 18:15:12 +0900

    Incur

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,64 @@
+$(function(){
+
+  function increHTML(user){
+    var html =`
+              <div class="chat-group-user clearfix incre">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+              </div>
+    `
+    return html
+  }
+
+  function add(name,id){
+    var html = `
+              <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                <input name='group[user_ids][]' type='hidden' value="${id}">
+                <p class='chat-group-user__name'>${name}</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>
+    `
+    return html
+  }
+
+
+
+  $("#user-search-field").on("keyup",function(){
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url:'/users',
+      data: {keyword: input},
+      dataType: 'json'
+    })
+    .done(function(data){
+      $('#user-search-result').empty();
+      data.forEach(function(user){
+      var html = increHTML(user);
+      $('#user-search-result').append(html);
+      })
+    })
+    .fail(function(){
+    alert('ユーザー検索に失敗しました')
+    });
+  });
+
+
+
+
+  $('#user-search-result').on('click','.user-search-add.chat-group-user__btn.chat-group-user__btn--add' , function(){
+    name = this.dataset.userName
+    id = this.dataset.userId
+    incre = add(name,id)
+    console.log(incre)
+    $('.incre').remove();
+    $('#chat-group-users').append(incre)
+  })
+
+  $('#chat-group-users').on('click','.js-remove-btn',function(){
+    $(this).parent().remove();
+  })
+
+
+});
+
